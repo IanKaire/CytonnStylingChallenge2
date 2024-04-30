037f912a37976fae0d1f427ba84824b1a2cf6c5e
Ian Kaire <iankaire@gmail.com>
2024-04-30 12:16:34 +0300

    completed responsiveness

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ const searchBar = document.getElementById('search-bar');
 searchIcon.addEventListener('click', () => {
   searchBar.style.display = searchBar.style.display === 'none' ? 'block' : 'none';
 });
+
+const backdrop = document.querySelector(".backdrop");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  backdrop.classList.remove("open");
+});
+
+toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
